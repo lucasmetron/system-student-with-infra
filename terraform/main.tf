a794825ff813db3c9f1c2435834fbd70d85fdf4e
Lucas Rosa <lucasmetron@gmail.com>
2026-09-17 13:50:32 -0300

    feat: add Terraform configuration for Azure infrastructure and virtual machine

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,8 @@ resource "tls_private_key" "ssh_key" {
 }
 
 resource "local_file" "private_key" {
-  content         = tls_private_key.ssh_key.private_key_pem
-  filename        = "${path.module}/id_rsa.pem"
-  file_permission = "0600"
+  content  = tls_private_key.ssh_key.private_key_pem
+  filename = "${path.module}/id_rsa.pem"
 }
 
 # 2. Grupo de Recursos (Resource Group)
